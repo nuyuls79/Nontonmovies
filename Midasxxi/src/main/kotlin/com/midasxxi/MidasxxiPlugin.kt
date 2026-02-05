e444bb89e96d6be8c1c4fd1cdb02d056ae338573
GilangAditama <aditamag84@gmail.com>
2026-02-05 10:51:07 +0700

    Update MidasxxiPlugin.kt

--- Midasxxi/src/main/kotlin/com/midasxxi/MidasxxiPlugin.kt
+++ Midasxxi/src/main/kotlin/com/midasxxi/MidasxxiPlugin.kt
@@ -1,15 +1,15 @@
 package com.midasxxi
 
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.registerMainAPI
+import com.lagradost.cloudstream3.plugins.registerExtractorAPI
 
 @CloudstreamPlugin
-class MidasxxiPlugin: Plugin() {
-    override fun load(context: Context) {
-        Midasxxi.context = context
-        registerMainAPI(Midasxxi()) 
-        registerExtractorAPI(Playcinematic()) 
+class MidasxxiPlugin : Plugin() {
+
+    override fun load() {
+        registerMainAPI(Midasxxi())
+        registerExtractorAPI(Playcinematic())
     }
 }
