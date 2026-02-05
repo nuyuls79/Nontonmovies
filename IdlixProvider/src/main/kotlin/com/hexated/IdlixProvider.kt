caf2b74aea8c115c1487b361e4cbec0d9ec09b0b
CodeSanzz <mlus01089@gmail.com>
2026-02-06 05:06:36 +0700

    update IdlixProvider

--- IdlixProvider/src/main/kotlin/com/hexated/IdlixProvider.kt
+++ IdlixProvider/src/main/kotlin/com/hexated/IdlixProvider.kt
@@ -19,7 +19,7 @@ class IdlixProvider : MainAPI() {
     companion object {
         var context: android.content.Context? = null
     }
-    override var mainUrl = "https://tv11.idlixku.com"
+    override var mainUrl = "https://tv12.idlixku.com"
     private var directUrl = mainUrl
     override var name = "Idlix🎄"
     override val hasMainPage = true
