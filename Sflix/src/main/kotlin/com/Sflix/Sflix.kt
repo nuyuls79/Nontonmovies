2efcf1b9e8eb4882b10d9d72bec02193011340b8
CodeSanzz <mlus01089@gmail.com>
2026-02-10 15:23:08 +0700

    Update Sflix source

--- Sflix/src/main/kotlin/com/Sflix/Sflix.kt
+++ Sflix/src/main/kotlin/com/Sflix/Sflix.kt
@@ -15,7 +15,7 @@ class Sflix : MainAPI() {
     override var mainUrl = "https://sflix.film"
     private val apiUrl = "https://sflix.film"
     override val instantLinkLoading = true
-    override var name = "Sflix"
+    override var name = "Sflix🧲"
     override val hasMainPage = true
     override val hasQuickSearch = true
     override var lang = "id"
