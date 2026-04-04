120097a2596e0a1a51aac25336fe0e9e9d17ab02
nuyuls79 <nuyulshare001@gmail.com>
2026-04-04 12:28:25 +0700

    Change Recloudstream gradle dependency to master-SNAPSHOT
    Updated Recloudstream gradle dependency to use master-SNAPSHOT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2") // Versi AGP terbaru
-        classpath("com.github.recloudstream:gradle:cce1b8d84d") // SHA commit stabil
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0") // Cocok Cloudstream pre-release
     }
 }
