e8dcc8571c9ff358594cf09089641f1d6a0d7f34
nuyuls79 <nuyulshare001@gmail.com>
2026-04-04 13:06:09 +0700

    Fix import statement for Okru extractor

--- Animexin/src/main/kotlin/com/Animexin/AnimexinProvider.kt
+++ Animexin/src/main/kotlin/com/Animexin/AnimexinProvider.kt
@@ -3,7 +3,7 @@ package com.Animexin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.extractors.Dailymotion
-import com.lagradost.cloudstream3.extractors.Okrulink
+import com.lagradost.cloudstream3.extractors.Okru
 
 @CloudstreamPlugin
 class AnimexinProvider: BasePlugin() {
